ef4c7779c2ec28356cd37c700764361413b99cd3
wujibear <hello@wujibear.studio>
2024-01-10 16:32:22 -0800

    Add export command

---
+++ src/commands/export.ts
@@ -0,0 +1,37 @@
+import {Args } from '@oclif/core'
+import { NamespacedCommand } from '../NamespacedCommand.js'
+import * as fs from 'fs'
+import { closestPath, ShellFileTypes } from '../utils/files.js'
+import {confirm} from '@inquirer/prompts'
+import FileAPI from '../api/files.js'
+import { template } from '../api/templates.js'
+
+export default class Export extends NamespacedCommand {
+  static description = 'creates a command line export'
+
+  static examples = [
+    '<%= config.bin %> <%= command.id %> export_name "some kind of content"',
+  ]
+
+  static args = {
+    name: Args.string({description: 'the call name of your export (case insensitive)', required: true}),
+    content: Args.string({description: 'the content of your export', required: true}),
+  }
+
+  public async run(): Promise<void> {
+    const {args, flags} = await this.parse(Export)
+    const {name, content} = args
+    const {namespace} = flags
+    const filePath = closestPath({namespace, type: ShellFileTypes.export, name})
+
+    if (fs.existsSync(filePath)) {
+      const overwrite = await confirm({
+        message: `The export '${name}' exists already. Overwrite?`,
+        default: true
+      })
+      if (!overwrite) return
+    }
+    const fileContent = await template.render('export', {exportName: name, content})
+    new FileAPI(filePath).write(fileContent)
+  }
+}
