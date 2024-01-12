a35c6582e5058944cc06237aec370ab178873580
wujibear <hello@wujibear.studio>
2024-01-11 19:20:04 -0800

    Add function action

--- src/NamespacedCommand.ts
+++ src/NamespacedCommand.ts
@@ -1,11 +1,12 @@
 import {Command, Flags} from '@oclif/core'
+import { settings } from './api/config.js';
 
 export abstract class NamespacedCommand extends Command {
   static baseFlags = {
     namespace: Flags.string({
       char: 'n',
       description: 'namespace directory to use',
-      default: 'user',
+      default: settings.defaultNamespace,
     }),
   };
 }
---
+++ src/commands/function.ts
@@ -0,0 +1,43 @@
+import {Args, Flags} from '@oclif/core'
+import { Exec } from '../api/shell.js'
+import { ShellFileTypes, closestPath } from '../utils/files.js'
+import { NamespacedCommand } from '../NamespacedCommand.js'
+import * as fs from 'fs'
+import FileAPI from '../api/files.js'
+import { template } from '../api/templates.js'
+
+export default class Edit extends NamespacedCommand {
+  static description = 'sets default <%= config.bin %> configurations'
+
+  static examples = [
+    '<%= config.bin %> <%= command.id %> -n=mycompany',
+  ]
+
+  static flags = {
+    content: Flags.string({
+      char: 'c',
+      description: 'content of the function. Skips opening the editor when given',
+    }),
+    description: Flags.string({
+      char: 'd',
+      description: 'A description of what your function does',
+    })
+  }
+
+  static args = {
+    name: Args.string({description: 'filename to edit'}),
+  }
+
+  public async run(): Promise<void> {
+    const {args, flags} = await this.parse(Edit)
+    const {namespace, content, description} = flags
+    const {name} = args
+    const filePath = closestPath({name, type: ShellFileTypes.function, namespace})
+    const fileContent = await template.render('function', {functionName: name, content, description})
+    if (!fs.existsSync(filePath)) new FileAPI(filePath).write(fileContent)
+    if (content) return
+
+    const command = `${process.env.EDITOR} ${filePath}`
+    Exec(command) // Vim fails, probs needs specific thread?
+  }
+}
