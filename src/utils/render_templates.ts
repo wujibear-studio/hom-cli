e86461e9bbb6a26b24474f65bee62c04ce0e6240
Brandon McKay <t-brandon.mckay@gsoft.com>
2024-01-24 09:13:52 -0800

    Adjust empty space on templates

--- src/utils/render_templates.ts
+++ src/utils/render_templates.ts
@@ -1,17 +1,16 @@
-export const aliasFile = `
-#!/usr/bin/env bash
+export const aliasFile = `#!/usr/bin/env bash
 #description: {{description | default: content}}
+
 alias -- {{aliasName}}="{{content}}"
 `
 
-export const exportFile = `
-#!/usr/bin/env bash
+export const exportFile = `#!/usr/bin/env bash
 #description: {{description | default: content}}
+
 export {{exportName | upcase}}="{{content}}"
 `
 
-export const functionFile = `
-#!/usr/bin/env bash
+export const functionFile = `#!/usr/bin/env bash
 #description: {{description | default: functionName}}
 
 function {{functionName}}() {
@@ -19,15 +18,13 @@ function {{functionName}}() {
 }
 `
 
-export const partialFile = `
-#!/usr/bin/env bash
+export const partialFile = `#!/usr/bin/env bash
 #description: {{description | default: partialName}}
 
 {{content}}
 `
 
-export const scriptFile = `
-#!/usr/bin/env bash
+export const scriptFile = `#!/usr/bin/env bash
 #description: {{description | default: scriptName}}
 
 {{content}}
