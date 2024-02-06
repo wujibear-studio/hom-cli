c5a34c6bd92d071f853809ed24828c3c5063fd69
wujibear <hello@wujibear.studio>
2024-02-05 18:13:39 -0800

    Update sourcing template

--- config_templates/source_namespaces.sh
+++ config_templates/source_namespaces.sh
@@ -3,16 +3,15 @@
 function source_namespaces() {
   setopt +o nomatch # Prevents error messages when there's no folder or files within them
   setopt BASH_REMATCH # allows zsh to handle regex same as bash
-  namespaces=$(cd $HOM_DIR && echo */)
 
-  for namespace in $namespaces
+  for namespace in $HOM_DIR/*/
   do
-    [[ $namespace =~ [a-zA-Z0-9]* ]]
+    [[ $namespace =~ \.?[a-zA-Z0-9]*\/$ ]]
     stripped_name="${BASH_REMATCH[1]}"
 
-    if [[ "$stripped_name/" == $namespace && $stripped_name != 'core' ]]
+    if [[ $stripped_name && $stripped_name != '.core' ]]
     then
-      source_dir "$HOM_DIR/$stripped_name"
+      source_dir "$HOM_DIR$stripped_name"
     fi
   done
 }
